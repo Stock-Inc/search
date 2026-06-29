58c43631439a27cfd2e0752a50ef082efae55f67
lZh1r <electroxhackerpig09@gmail.com>
2026-06-29 17:36:59 +0300

    upload page drag and drop logic

--- frontend/src/components/pages/Upload.tsx
+++ frontend/src/components/pages/Upload.tsx
@@ -1,7 +1,61 @@
+import { UploadIcon } from "lucide-react";
+import { Card } from "../ui/card";
+import { useRef, useState } from "react";
+
 export default function Upload() {
+    const [isDragging, setIsDragging] = useState(false);
+    const dragCount = useRef(0);
+    const [files, setFiles] = useState(new Map());
+    
+    function handleDragEnter(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        dragCount.current++;
+        setIsDragging(true);
+    }
+    
+    function handleDragLeave(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        dragCount.current--;
+        if (dragCount.current === 0) {
+            setIsDragging(false);
+        }
+    }
+    
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+    }
+    
+    function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setIsDragging(false);
+        const transferredFiles = e.dataTransfer.files;
+        setFiles(s => {
+            for (let i = 0; i < transferredFiles.length; i++) {
+                const file = transferredFiles.item(i);    
+                s.set(file.name, file);
+            }
+            return s;
+        });
+    }
+    
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center">
-            <h1>Hello</h1>
+            <Card
+                className="p-10 border-2 flex flex-col items-center space-y-8"
+                onDragEnter={handleDragEnter}
+                onDragLeave={handleDragLeave}
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+            >
+                <p className="text-2xl">Upload a file for scanning</p>
+                <UploadIcon className={`transition-all size-20 ${isDragging && "scale-125"}`} />
+                <p className="text-xl underline">Drag and drop or click to select</p>
+            </Card>
+            <input
+                type="file"
+                hidden
+                multiple
+            />
         </div>
     )
 }
